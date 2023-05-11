c23b3517ac355b20e8b889e6a17169f6ae5674c9
Hasan Hüseyin Demir <hasan.01imam01@gmail.com>
2023-05-11 11:21:40 +0300

    Sql Güncelleme

--- WindowsFormsApp1/urunler.Designer.cs
+++ WindowsFormsApp1/urunler.Designer.cs
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(urunler));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txtkod = new System.Windows.Forms.TextBox();
+            this.txtsatiş = new System.Windows.Forms.TextBox();
+            this.txtaliş = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,44 +42,48 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Azure;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(221, 30);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(440, 297);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
-            // textBox1
+            // txtad
             // 
-            this.textBox1.Location = new System.Drawing.Point(93, 57);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtad.Location = new System.Drawing.Point(93, 57);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 20);
+            this.txtad.TabIndex = 1;
             // 
-            // textBox2
+            // txtkod
             // 
-            this.textBox2.Location = new System.Drawing.Point(93, 93);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.txtkod.Location = new System.Drawing.Point(93, 93);
+            this.txtkod.Name = "txtkod";
+            this.txtkod.Size = new System.Drawing.Size(100, 20);
+            this.txtkod.TabIndex = 1;
             // 
-            // textBox3
+            // txtsatiş
             // 
-            this.textBox3.Location = new System.Drawing.Point(93, 166);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 1;
+            this.txtsatiş.Location = new System.Drawing.Point(93, 166);
+            this.txtsatiş.Name = "txtsatiş";
+            this.txtsatiş.Size = new System.Drawing.Size(100, 20);
+            this.txtsatiş.TabIndex = 1;
             // 
-            // textBox4
+            // txtaliş
             // 
-            this.textBox4.Location = new System.Drawing.Point(93, 131);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 1;
+            this.txtaliş.Location = new System.Drawing.Point(93, 131);
+            this.txtaliş.Name = "txtaliş";
+            this.txtaliş.Size = new System.Drawing.Size(100, 20);
+            this.txtaliş.TabIndex = 1;
             // 
             // label1
             // 
@@ -118,47 +123,78 @@
             // 
             // button1
             // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.Location = new System.Drawing.Point(667, 30);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.Size = new System.Drawing.Size(86, 58);
             this.button1.TabIndex = 5;
             this.button1.Text = "SİL";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Cursor = System.Windows.Forms.Cursors.SizeWE;
             this.button2.Location = new System.Drawing.Point(25, 208);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.Size = new System.Drawing.Size(75, 58);
             this.button2.TabIndex = 6;
             this.button2.Text = "GÜNCELLE";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button3.Location = new System.Drawing.Point(140, 208);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 33);
+            this.button3.Size = new System.Drawing.Size(75, 58);
             this.button3.TabIndex = 6;
             this.button3.Text = "İPTAL";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(667, 280);
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Location = new System.Drawing.Point(667, 260);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 47);
+            this.button4.Size = new System.Drawing.Size(86, 67);
             this.button4.TabIndex = 5;
             this.button4.Text = "Ana Sayfa";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(561, 3);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 1;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(515, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Kod ara";
             // 
             // urunler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 460);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -166,12 +202,14 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtaliş);
+            this.Controls.Add(this.txtsatiş);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.txtkod);
+            this.Controls.Add(this.txtad);
             this.Controls.Add(this.dataGridView1);
             this.Name = "urunler";
             this.Text = "urunler";
@@ -185,10 +223,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txtkod;
+        private System.Windows.Forms.TextBox txtsatiş;
+        private System.Windows.Forms.TextBox txtaliş;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -197,5 +235,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
     }
 }
--- WindowsFormsApp1/urunler.cs
+++ WindowsFormsApp1/urunler.cs
@@ -33,7 +33,15 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            string cümle = "update urunekle set urunadi=@urunadi,urunaliş=@urunaliş,urunsatiş=@urunsatiş where urunkodu=@urunkodu";
+            SqlCommand komut2 = new SqlCommand();
+            komut2.Parameters.AddWithValue("@urunadi", txtad.Text);
+            komut2.Parameters.AddWithValue("@urunkodu", txtkod.Text);
+            komut2.Parameters.AddWithValue("@urunaliş", txtaliş.Text);
+            komut2.Parameters.AddWithValue("@urunsatiş", txtsatiş.Text);
+            marketotomasyon.ekle_sil_güncelle(komut2, cümle);
+            foreach (Control item in Controls) if (item is TextBox) item.Text="";
+            Yenilelistele();
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -42,6 +50,24 @@ namespace WindowsFormsApp1
             this.Hide();
             form1gecis.ShowDialog();
             this.Close();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            string cümle = "select *from urunekle where urunkodu like '%"+textBox5.Text+"%'";
+            SqlDataAdapter adtr2 = new SqlDataAdapter();
+            dataGridView1.DataSource=marketotomasyon.listele(adtr2, cümle);
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow satir = dataGridView1.CurrentRow;
+            txtad.Text=satir.Cells[0].Value.ToString();
+            txtkod.Text=satir.Cells[1].Value.ToString();
+            txtaliş.Text=satir.Cells[2].Value.ToString();
+            txtsatiş.Text=satir.Cells[3].Value.ToString();
+
+
         }
     }
 }
